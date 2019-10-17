fae70b78f40c833c84df042e7afcb67a1890192d
Andre Wong <andre@andre-wong.com>
2019-10-18 00:34:24 +1000

    Added roles

---
+++ src/shared/roles.ts
@@ -0,0 +1,56 @@
+interface Role {
+  description: string
+  side: 'mafia' | 'village' // Which side they win with
+  appearsAs: 'mafia' | 'village' // Side revealed on 'see' actions
+  actions: {
+    firstNight?: ActionType
+    day?: ActionType
+    night?: ActionType
+  }
+}
+
+type ActionType =
+  | 'inform' // Reveal role to others at night
+  | 'lynch' // Vote to lynch target at day
+  | 'kill' // Vote to kill target at night
+  | 'see' // See a target's side
+  | 'heal' // Prevent a kill on target
+
+export const ROLES: Readonly<{[name: string]: Role}> = Object.freeze({
+  villager: {
+    description: '',
+    side: 'village',
+    appearsAs: 'village',
+    actions: {
+      day: 'lynch',
+    },
+  },
+  mafia: {
+    description: '',
+    side: 'mafia',
+    appearsAs: 'mafia',
+    actions: {
+      firstNight: 'inform',
+      day: 'lynch',
+      night: 'kill',
+    },
+  },
+  detective: {
+    description: '',
+    side: 'village',
+    appearsAs: 'village',
+    actions: {
+      day: 'lynch',
+      night: 'see',
+    },
+  },
+  nurse: {
+    description: '',
+    side: 'village',
+    appearsAs: 'village',
+    actions: {
+      day: 'lynch',
+      night: 'heal',
+    },
+  },
+})
