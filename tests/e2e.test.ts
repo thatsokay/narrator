9e1fc103cf9d025023082801e4962fa827f87d5f
Andre Wong <andre@andre-wong.com>
2020-02-17 00:06:00 +1000

    Added e2e test

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,5 +1,6 @@
-import puppeteer from 'puppeteer'
 import {Server} from 'http'
+import puppeteer from 'puppeteer'
+import R from 'ramda'
 
 import app from '../src/server/server'
 
@@ -23,5 +24,33 @@ describe('e2e', () => {
     const page = await browser.newPage()
     await page.goto('http://localhost:3000')
     await page.waitFor('#create-player-name', {timeout: 100})
+  })
+
+  test('start game', async done => {
+    const pages = await Promise.all(R.range(0, 6).map(_ => browser.newPage()))
+    await Promise.all(pages.map(page => page.goto('http://localhost:3000')))
+    await (await pages[0].waitFor('#create-player-name')).type('player0\n')
+    const roomIdHandle = await pages[0].waitFor('h1')
+    const roomId = (await roomIdHandle.evaluate(
+      node => node.textContent,
+    )) as string
+    expect(roomId.length).toBe(4)
+    await Promise.all(
+      pages.slice(1).map(async (page, i) => {
+        const roomHandle = await page.waitFor('#join-room-id')
+        await roomHandle.type(roomId)
+        const nameHandle = await page.waitFor('#join-player-name')
+        await nameHandle.type(`player${i + 1}\n`)
+      }),
+    )
+    await Promise.all(
+      pages.map(async page => {
+        const statusHandle = await page.waitFor('h2')
+        expect(await statusHandle.evaluate(node => node.textContent)).toBe(
+          'waiting',
+        )
+      }),
+    )
+    done()
   })
 })
